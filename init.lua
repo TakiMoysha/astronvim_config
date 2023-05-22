6aa5d515f94cff7eaf39a30d12cdccea4a970575
TakiMoysha <mpwema782@gmail.com>
2023-05-22 13:51:06 +0400

    setup denols and tsserver

--- init.lua
+++ init.lua
@@ -1,12 +1,29 @@
 return {
-  n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
+  lsp = {
+    config = {
+      denols = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
+        return opts
+      end,
+      tsserver = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern("package.json")
+        return opts
+      end,
+    },
+    servers = {
+      "tsserver",
+      "denols",
+      "vuels",
+      "rust_analyzer",
+    },
+    ["server-settings"] = {
+      denols = {
+      },
+      tsserver = {
+      },
+      rust_analyzer = {
+        
+      }
+    }
   },
-  t = {
-    -- settings a mapping to false will disable it
-  },
-  v = {
-
-  }
 };
