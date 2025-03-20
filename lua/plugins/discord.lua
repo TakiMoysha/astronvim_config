631b14b685f6a275d5d88ede6f9aae9c4e201614
TakiMoysha <takimoysha@users.noreply.github.com>
2025-03-20 13:03:01 +0100

    added cord.nvim for discord integrations, updated lock file

---
+++ lua/plugins/discord.lua
@@ -0,0 +1,45 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+---@type LazySpec
+return {
+  {
+    "vyfor/cord.nvim",
+    branch = "client-server",
+    build = ":Cord udpate",
+    event = "VeryLazy",
+    enabled = true,
+    opts = {},
+    config = function()
+      require("cord").setup {
+        timestamp = {
+          enalbed = true,
+          reset_on_idle = false,
+          reset_on_change = false,
+        },
+        editor = {
+          client = "astronvim",
+        },
+        idle = {
+          unidle_on_focus = false,
+          ignore_focus = true,
+          tooltip = "💤",
+        },
+        text = {
+          viewing = "Viewing {}",
+          editing = function(opts)
+            if opts.filetype == "rust" then return "🦀 Rusting..." end
+            return "Editing " .. opts.filename
+          end,
+          lsp = "LSP:{}",
+          dashboard = "Dashboard:Home",
+        },
+        buttons = {
+          {
+            label = function(opts) return opts.repo_url and "Repository" or "Website" end,
+            url = function(opts) return opts.repo_url or "https://github.com/takimoysha" end,
+          },
+        },
+      }
+    end,
+  },
+}
