c4ef900676a501ef1ee8e4f11fd1136546449346
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-06-25 11:59:37 +0200

    added ranger, updated

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -48,6 +48,12 @@ return {
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
+        -- ranger
+        ["<Leader>tr"] = {
+          function() require("astrocore").toggle_term_cmd { cmd = "ranger", direction = "float" } end,
+          desc = "ToggleTerm ranger",
+        },
+
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
           function()
