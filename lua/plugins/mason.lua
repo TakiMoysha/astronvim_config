c63e4c6dcee33932eadede90043c0ebb5c45c41e
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-09-15 22:00:04 +0200

    added typos_lsp for spellcheck

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,6 +10,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         -- "tsserver", -- WARN: try starting in python projects
+        "typos_lsp", -- linting spellcheck
         "lua_ls",
         -- # rust
         -- "tombi",
