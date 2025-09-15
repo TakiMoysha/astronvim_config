e617d4ae3dcd411634244ab3314362721e152135
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-09-15 02:27:21 +0200

    updated executor config

--- lua/plugins/executor.lua
+++ lua/plugins/executor.lua
@@ -14,9 +14,25 @@ return {
     "ExecutorToggleDetail",
     "ExecutorReset",
   },
-  opts = function(_, opts)
-    if not opts.ensure_installed then opts.ensure_installed = {} end
-  end,
+  opts = {
+    use_split = true,
+    popup = {
+      -- Sets the width of the popup to 3/5ths of the screen's width.
+      width = math.floor(vim.o.columns * 6 / 10),
+      -- Sets the height to almost full height, allowing for some padding.
+      height = vim.o.lines - 20,
+      -- Border styles
+      border = {
+        padding = {
+          top = 1,
+          bottom = 1,
+          left = 0,
+          right = 0,
+        },
+        style = "rounded",
+      },
+    },
+  },
   dependencies = {
     { "MunifTanjim/nui.nvim" },
     {
@@ -28,8 +44,11 @@ return {
         opts.mappings.n["<Leader>Ee"] = { "<Cmd>ExecutorRun<CR>", desc = "Execute" }
         opts.mappings.n["<Leader>Es"] = { "<Cmd>ExecutorSetCommand<CR>", desc = "Set command" }
         opts.mappings.n["<Leader>Et"] = { "<Cmd>ExecutorToggleDetail<CR>", desc = "Toggle Detail" }
+        opts.mappings.n["<F8>"] = { "<Cmd>ExecutorToggleDetail<CR>", desc = "Toggle Detail" }
         opts.mappings.n["<Leader>ES"] = { "<Cmd>ExecutorSwapToSplit<CR>", desc = "Swap to Split" }
         opts.mappings.n["<Leader>EP"] = { "<Cmd>ExecutorSwapToPopup<CR>", desc = "Swap to Popup" }
+        opts.mappings.n["<Leader>Eh"] = { "<Cmd>ExecutorShowHistory<CR>", desc = "Show history" }
+        opts.mappings.n["<Leader>Er"] = { "<Cmd>ExecutorReset<CR>", desc = "Clear the stored commands and output" }
         opts.mappings.n["<Leader>E"] = { desc = "Executor" }
       end,
     },
